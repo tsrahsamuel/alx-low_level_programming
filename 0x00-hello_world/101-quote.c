88258dfbb160603e15d22c24b48a58c06b0173a6
tsrahsamuel <tsrahsamuel@gmail.com>
2022-06-30 14:32:44 -0700

    hmm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - Prints quote to the standard error
- *
- * Return: zero on success
- *
+ * main - prints string to standard error
+ * Return: Return 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	fprintf(stderr, "%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
