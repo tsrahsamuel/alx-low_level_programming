0cfaa7028e16db4b448c9660cde4a22112588a4b
tsrahsamuel <tsrahsamuel@gmail.com>
2022-06-30 13:31:04 -0700

    buggy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Prints quote
- *
- *   *
- *
- *    * Return: zero on success
- *
- *     *
- *
- *      */
-
+ * main - Prints quote
+ * Return: zero on success
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
