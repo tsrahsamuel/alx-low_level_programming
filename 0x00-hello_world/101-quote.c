da43e608dae346cc2baeac2697a2769fec404a4c
tsrahsamuel <tsrahsamuel@gmail.com>
2022-06-30 14:23:16 -0700

    please

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- *main - Entry point
- *Return: 0
+ * main - Prints quote to the standard error
+ *
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
