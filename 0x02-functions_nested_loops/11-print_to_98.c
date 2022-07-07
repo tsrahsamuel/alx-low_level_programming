e1fb90594fe53c02c23ccaca891a5e95e0592653
tsrahsamuel <tsrahsamuel@gmail.com>
2022-07-07 16:36:45 -0700

    ol

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_to_98- imprime todos los numeros naturales hasta el 98
+ *@n: parametro inicial
+ *Return: always 0.
+ */
+void print_to_98(int n)
+{
+	int stop = 98;
+
+	if (n <= stop)
+		for (; n <= stop; n++)
+		{
+			printf("%d", n);
+			n != stop ? printf(", ") : printf("\n");
+		}
+	else
+		for (; n >= stop; n--)
+		{
+			printf("%d", n);
+			n != stop ? printf(", ") : printf("\n");
+												}
+}
