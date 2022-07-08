bbfd920d57482ab24715ce543fa80c7c97aac313
tsrahsamuel <tsrahsamuel@gmail.com>
2022-07-08 10:19:02 -0700

    omsdf

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test function 
+ * return zero
+ */
+int main(void)
+{	
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+
+cnwiosdjncjcn oscnosdcnsdc
