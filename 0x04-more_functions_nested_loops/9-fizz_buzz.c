c244b589636861ab2f2e392bac998706dcc50364
tsrahsamuel <tsrahsamuel@gmail.com>
2022-07-08 09:02:33 -0700

    sfvnskdv

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -20,7 +20,14 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 		else
 		{
