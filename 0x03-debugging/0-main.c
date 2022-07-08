f84b15fc38c987abb6917c45aacee1b6b0cd5c0d
tsrahsamuel <tsrahsamuel@gmail.com>
2022-07-08 10:30:18 -0700

    lsfcse

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
-#include "main.h"
+#include "main."
 
 /**
- * main - test function 
- * return zero
+ *main - test function
+ *Return: 0
  */
 int main(void)
-{	
+{
 	int i;
 
-	i=0;
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
